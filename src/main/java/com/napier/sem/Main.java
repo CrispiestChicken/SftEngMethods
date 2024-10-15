fd0998e68a0954421a889d189b62cf86965f1feb
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2024-10-15 18:08:23 +0100

    Seperated out Database into its own class and used it in the main method.

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -6,63 +6,13 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        Database db = new Database();
 
-        // Trys to load database driver and ends if it can't
-        try
-        {
-            // Load Database driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
-            System.out.println("Could not load SQL driver");
-            System.exit(-1);
-        }
+        // Connect to database
+        db.connect();
 
-
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
-        for (int i = 0; i < retries; ++i)
-        {
-            System.out.println("Connecting to database...");
-            try
-            {
-                // Wait a bit for db to start
-                Thread.sleep(30000);
-                // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
-                System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
-                break;
-            }
-            // If fails to connect and gives SQLException
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
-                System.out.println(sqle.getMessage());
-            }
-            // If fails to connect and gives an InterruptedException
-            catch (InterruptedException ie)
-            {
-                System.out.println("Thread interrupted? Should not happen.");
-            }
-        }
-
-        // Close the database when it gets to here if the connection is running.
-        if (con != null)
-        {
-            try
-            {
-                // Close connection
-                con.close();
-            }
-            catch (Exception e)
-            {
-                System.out.println("Error closing connection to database");
-            }
-        }
+        // Disconnect from database
+        db.disconnect();
     }
 }
