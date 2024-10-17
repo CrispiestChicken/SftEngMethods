e4ae3b7bbced005182ba118425e86770678991c2
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2024-10-17 16:34:56 +0100

    App now gets all the employees salarys along with their names and employee number

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,5 +1,6 @@
 package com.napier.sem;
 
+import java.util.ArrayList;
 
 public class Main
 {
@@ -10,10 +11,12 @@ public class Main
 
         // Connect to database
         a.connect();
-        // Get Employee
-        Employee emp = a.getEmployee(255530);
-        // Display results
-        a.displayEmployee(emp);
+
+        // Extract employee salary information
+        ArrayList<Employee> employees = a.getAllSalaries();
+
+        // Test the size of the returned data - should be 240124
+        System.out.println(employees.size());
 
         // Disconnect from database
         a.disconnect();
